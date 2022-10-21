1baaf2b6db6ddcf16c7f014bd41cb998dd5e06bb
TinaLacatis <danatina.dt@gmail.com>
2022-10-21 22:08:05 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Contacts</h1>
+
+      <Card name="Beyonce" img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" tel="+123 456 789" email="b@beyonce.com" />
+      <Card name="Jack Bauer" img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg" tel="+987 654 321" email="jack@nowhere.com" />
+      <Card name="Chuck Norris" img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png" tel="+918 372 574" email="gmail@chucknorris.com" />
     </div>
   );
 }
 
 export default App;
+
+function Card(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img
+        src={props.img}
+        alt="avatar_img"
+      />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  )
+}
