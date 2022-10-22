6ffaae9c2d21d916c3333f33a8e5b35e708c97e0
tina <danatina.dt@gmail.com>
2022-10-22 13:31:56 +0200

    mapping the data

--- src/components/App.js
+++ src/components/App.js
@@ -2,29 +2,23 @@ import contacts from '../contacts'
 import Card from './Card'
 import Avatar from './Avatar';
 
+function createCard(contact){
+  return <Card 
+  key = {contact.id}
+  name = {contact.name}
+  img = {contact.imgURL}
+  tel = {contact.phone}
+  email = {contact.email}
+  />
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Avatar img="https://www.rd.com/wp-content/uploads/2020/09/GettyImages-916193692.jpg?fit=700,453" />
-      <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      />
+
+      {contacts.map(createCard)}
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -5,6 +5,7 @@ export default function Card(props) {
     return (
         <div className="card">
             <div className="top">
+                <p>{props.id}</p>
                 <h2 className="name">{props.name}</h2>
                 <Avatar img={props.img} />
             </div>
